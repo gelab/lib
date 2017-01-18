0d603331cc977402402cf5e0723e0f2b3025b116
gs <gs@expertoption.com>
2017-01-18 08:24:59 +0200

    lib 004

--- cpy_distutils.py
+++ cpy_distutils.py
@@ -21,6 +21,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
+# master 000
+
 """Implements the DistUtils command 'build_ext'
 """
 
