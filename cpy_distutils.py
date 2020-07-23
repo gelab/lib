a614e14dafa65afaef508f73531c0cc131a62df1
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-23 04:41:43 +0300

    Migrate from master and slave jargon

--- cpy_distutils.py
+++ cpy_distutils.py
@@ -21,7 +21,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
-# master 000
+# main 000
 
 """Implements the DistUtils command 'build_ext'
 """
